535506db4c084d28770fc11141bc0e95ca9de4c2
unknown <green.koopa667@gmail.com>
2017-10-11 01:16:51 -0400

    Wrapper function

---
+++ TSP.m
@@ -0,0 +1,10 @@
+function [ output_args ] = TSP( filename )
+% Wrapper function for TSP Project 4
+% Calls necessary functions to perform and output results of genetic
+% algorithm. Heavy duty work is in tsp_ga.m
+
+
+
+
+end
+
